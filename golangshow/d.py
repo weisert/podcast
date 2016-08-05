2e11d43972866f904fb44139dd328f71e34e6e5b
Igor Vayzert <weisert@yandex-team.ru>
2016-08-05 10:02:44 +0700

    GoLang Russian podcast from golangshow.com

---
+++ golangshow/d.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from subprocess import check_call
+
+def download(url, output_name=None):
+  try:
+    args = ['wget', url]
+    if output_name:
+      args.extend(['-O', output_name])
+    check_call(args)
+  except:
+    print 'Except:', url
+
+
+template = 'https://golangshow.com/cdn/episodes/{0:03d}.mp3'
+
+for i in range(1, 67):
+  file_url = template.format(i)
+  download(file_url)
+
+download('https://golangshow.com/cdn/episodes/055_xyz.mp3', '055.mp3')
+
